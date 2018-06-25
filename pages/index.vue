ae53bd24f2e921d426dcce8164459c0230297d25
dai <chamestica@gmail.com>
2018-06-25 13:53:13 +0900

    Activate form for Netlify

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    index
-  </div>
+  <form name="contact" method="post" netlify>
+    <input type="hidden" name="form-name" value="contact"/>
+    <label>Name <input type="text" name="name"/></label>
+    <label>Email <input type="email" name="email"/></label>
+    <button type="submit">Send</button>
+  </form>
 </template>
+
+<style>
+label {
+  display: block;
+}
+button {
+  display: block;
+}
+</style>
