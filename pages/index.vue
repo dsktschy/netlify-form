aaf6163d6603c32b5a5f05ae591ea6fcbfb90d23
dai <chamestica@gmail.com>
2018-06-27 04:53:00 +0900

    Use Ajax

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,37 @@
 <template>
-  <form name="contact" method="post" netlify>
-    <input type="hidden" name="form-name" value="contact"/>
-    <label>Name <input type="text" name="name"/></label>
-    <label>Email <input type="email" name="email"/></label>
+  <form ref="root" @submit.prevent="submit">
+    <label>Name:<input type="text" v-model="form.name" required/></label>
+    <label>Email:<input type="email" v-model="form.email" required/></label>
+    <label>Message:<textarea v-model="form.message" required/></label>
     <button type="submit">Send</button>
   </form>
 </template>
+
+<script>
+import axios from 'axios'
+import qs from 'qs'
+
+export default {
+  data () {
+    return {
+      form: {
+        'form-name': 'contact',
+        name: '',
+        email: '',
+        message: ''
+      }
+    }
+  },
+  methods: {
+    submit () {
+      axios.post(this.$refs.root.action, qs.stringify(this.form))
+        .then(() => {
+          alert('Thank you!')
+        })
+    }
+  }
+}
+</script>
 
 <style>
 label {
